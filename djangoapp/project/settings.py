5fc75f8a721c7abfa66523028b3197c3c5665778
Cleison F <faletacleison@gmail.com>
2025-11-01 16:49:57 -0300

    'Atualizações de segurança'

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR.parent / 'data' / 'web'
+
+#DOTENV
+load_dotenv(BASE_DIR.parent / 'dotenv_files' / '.env', override=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -48,6 +51,9 @@ INSTALLED_APPS = [
 
     # Summernote
     'django_summernote',
+
+    #  O Axes pode ser colocado em qualquer posição, mas é recomendado que seja o primeiro da lista.
+    'axes',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +64,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # AxesMiddleware runs during the reponse phase. It does not conflict
+    # with middleware that runs in the request phase like
+    # django.middleware.cache.FetchFromCacheMiddleware.
+    'axes.middleware.AxesMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -143,6 +154,15 @@ MEDIA_ROOT = DATA_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTHENTICATION_BACKENDS = [
+    # AxesStandaloneBackend should be the first backend in the AUTHENTICATION_BACKENDS list.
+    'axes.backends.AxesStandaloneBackend',
+
+    # Django ModelBackend is the default authentication backend.
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+
 SUMMERNOTE_CONFIG = {
     'summernote': {
         # Toolbar customization
@@ -169,3 +189,8 @@ SUMMERNOTE_CONFIG = {
     'attachment_filesize_limit': 30 * 1024 * 1024,
     'attachment_model': 'blog.PostAttachment',
 }
+
+AXES_ENABLED = True
+AXES_FAILURE_LIMIT = 4
+AXES_COOLOFF_TIME = 1  # 1 hora
+AXES_RESET_ON_SUCCESS = True
